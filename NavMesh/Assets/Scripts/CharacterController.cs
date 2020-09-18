bf8076507d01ab8ba52e9192af29decbaaf0a93f
Mikhail Sergeev <sergeevmisha16@gmail.com>
2020-09-18 11:13:17 +0300

    Learn AI using NavMesh

---
+++ NavMesh/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    [SerializeField] private GameObject target;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
