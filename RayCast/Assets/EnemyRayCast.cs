0c1238536901560329e4e90501374d5f72275acd
Mikhail Sergeev <sergeevmisha16@gmail.com>
2020-09-20 11:25:44 +0300

    Fps camera and controller

---
+++ RayCast/Assets/EnemyRayCast.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRayCast : MonoBehaviour
+{
+    [SerializeField] private int maxDistance = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
