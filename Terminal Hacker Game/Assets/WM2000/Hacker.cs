b347419e5a196d63c3b4af68ea08ae2e821f64ec
Mikhail Sergeev <sergeevmisha16@gmail.com>
2020-10-18 20:05:20 +0300

    First commit

---
+++ Terminal Hacker Game/Assets/WM2000/Hacker.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    int level; 
+
+    enum State
+    {
+        MainMenu, 
+        Password, 
+        Win
+    }
+
+    State currenState;
+
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    void OnUserInput(string input)     // Usefull thing!
+    {
+        if (input == "menu")
+        {
+            ShowMainMenu();
+        }
+        else if (currenState == State.MainMenu)
+        {
+            RunMainMenu(input);
+        }
+        else if (currenState == State.Password)
+        {
+            
+        }
+     
+    }
+
+    private void RunMainMenu(string input)
+    {
+        switch (input)
+        {
+            case "1": 
+                level = 1; 
+                StartGame(level); 
+                break;
+            case "2":
+                level = 2;
+                StartGame(level); break;
+            case "3":
+                level = 3;
+                StartGame(level); break;
+            default:
+                Terminal.WriteLine("Please, choose a valid level");
+                break;
+        }
+    }
+
+    private void StartGame(int level)
+    {
+        currenState = State.Password;
+        Terminal.WriteLine("Password:");
+    }
+
+    void ShowMainMenu()
+    {
+        currenState = State.MainMenu;
+        Terminal.ClearScreen();
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Press 3 for NASA");
+        Terminal.WriteLine("Enter your selection: ");
+    }
+
+
+}
