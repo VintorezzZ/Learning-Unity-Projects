2ff355b5252d7d4ed9976794ed2247bd0f89bfad
Mikhail Sergeev <sergeevmisha16@gmail.com>
2020-10-19 09:51:56 +0300

    Update Hacker.cs

--- Terminal Hacker Game/Assets/WM2000/Hacker.cs
+++ Terminal Hacker Game/Assets/WM2000/Hacker.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    const string menuHint = "Type 'menu' to return";
+    string[] level_1_passwords = { "books", "shelf", "aisle", "password", "font", "borrow" };
+    string[] level_2_passwords = { "prisoner", "handcuffs", "holster", "uniform", "arrest" };
+    string[] level_3_passwords = { "space", "rocket", "eath", "solar", "sun", "star" };
+
     int level;
     string password;
     enum State
@@ -48,48 +52,99 @@ public class Hacker : MonoBehaviour
         }
      
     }
-
- 
-
+    
     private void RunMainMenu(string input)
     {
-        switch (input)
+        bool isValidNumber = (input == "1" || input == "2" || input == "3");  // Cool thing!
+        if (isValidNumber)
         {
-            case "1": 
-                level = 1;
-                password = "donkey";
-                StartGame(level); 
-                break;
-            case "2":
-                level = 2;
-                password = "combo";
-                StartGame(level); break;
-            case "3":
-                level = 3;
-                StartGame(level); break;
-            default:
-                Terminal.WriteLine("Please, choose a valid level");
-                break;
+            level = int.Parse(input);
+            AskForPassword();
+        }
+        else
+        {
+            Terminal.WriteLine("Please, choose a valid level");
         }
     }
 
-    private void StartGame(int level)
+    private void AskForPassword()
     {
         currenState = State.Password;
-        Terminal.WriteLine("You have chosen level " + level);
-        Terminal.WriteLine("Enter your password:");
+        Terminal.ClearScreen();
+        SetRandomPassword();
+        Terminal.WriteLine("Enter your password, hint: " + password.Anagram());
+        Terminal.WriteLine(menuHint);
+    }
+
+    private void SetRandomPassword()
+    {
+        switch (level)
+        {
+            case 1:
+                password = level_1_passwords[Random.Range(0, level_1_passwords.Length)];
+                break;
+            case 2:
+                password = level_2_passwords[Random.Range(0, level_1_passwords.Length)];
+                break;
+            case 3:
+                password = level_3_passwords[Random.Range(0, level_1_passwords.Length)];
+                break;
+            default:
+                Terminal.WriteLine("Invalid level number");
+                break;
+        }
     }
 
     private void CheckPassword(string input)
     {
-        if (input == password) 
+        if (input == password)
         {
-            Terminal.WriteLine("Well done.");
+            DisplayWinScreen();
         }
         else
         {
-            Terminal.WriteLine("Wrong password.");
+            AskForPassword();
         }
     }
 
+    private void DisplayWinScreen()
+    {
+        currenState = State.Win;
+        Terminal.ClearScreen();
+        ShowLevelReward();
+        Terminal.WriteLine(menuHint);
+    }
+
+    private void ShowLevelReward()
+    {
+        switch (level)
+        {
+            case 1:
+                Terminal.WriteLine("Have a book...");
+                Terminal.WriteLine(@"
+
+    _______
+   /      //
+  /      //
+ /_____ //
+(______(/
+"
+                );
+                break;
+            case 2:
+                Terminal.WriteLine("You got a prison key");
+                break;
+            case 3:
+                Terminal.WriteLine(@"
+NASA
+
+");
+                Terminal.WriteLine("Welcome to NASA's internal system!");
+                break;
+            default:
+                Debug.LogError("Invalid level");
+                break;
+        }
+        
+    }
 }
