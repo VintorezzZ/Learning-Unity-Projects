0d6215f3779fe219106a5c709f1b67eb85a64c1c
Mikhail Sergeev <sergeevmisha16@gmail.com>
2020-10-19 15:52:58 +0300

    A lot changes

--- Project Boost/Assets/Scripts/Rocket.cs
+++ Project Boost/Assets/Scripts/Rocket.cs
@@ -1,11 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 1000;
+    [SerializeField] float rotationSpeed = 100;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
+
     Rigidbody rb;
     AudioSource audioSource;
+
+    enum State 
+    { 
+        Alive, 
+        Dying, 
+        Transcending 
+    }
+    State state = State.Alive;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -15,26 +28,97 @@ public class Rocket : MonoBehaviour
 
     void Update()
     {
-        ProcessInput();
+        if (state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
+        
     }
 
-    private void ProcessInput()
+    private void OnCollisionEnter(Collision collision)
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (state != State.Alive)
+            return;
+
+        switch (collision.gameObject.tag)
         {
-            rb.AddRelativeForce(Vector3.up * 1000 * Time.deltaTime);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
+            case "Friend":
+
+                break;
+
+            case "Finish":
+                StartSuccessSequence();
+                break;
+
+            default:
+                //kill
+                StartDeathSequence();
+                break;
         }
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        Invoke("LoadFirstLevel", 1f);
+    }
+
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        Invoke("LoadNextSceen", 1f);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextSceen()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene + 1);
+    }
+
+    private void RespondToRotateInput()
+    {
+        rb.freezeRotation = true;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward * 100 * Time.deltaTime);
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward * 100 * Time.deltaTime);
+            transform.Rotate(-Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
+
+        rb.freezeRotation = false;
+    }
+
+    private void RespondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ApplyThrust();
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void ApplyThrust()
+    {
+        rb.AddRelativeForce(Vector3.up * rcsThrust * Time.deltaTime);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
         }
     }
 }
