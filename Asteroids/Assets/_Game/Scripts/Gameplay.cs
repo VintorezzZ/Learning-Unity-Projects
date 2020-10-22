3414d992ba21171bcaac350f85330018ed2b95cc
Mikhail Sergeev <sergeevmisha16@gmail.com>
2020-10-22 13:43:36 +0300

    First commit

---
+++ Asteroids/Assets/_Game/Scripts/Gameplay.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameplay : MonoBehaviour
+{
+
+    // public GameObject bullet;
+    public GameObject asteroid;
+    // public GameObject asteroidContainer;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        
+        asteroid.SetActive(false);
+        CreateAsteroids(4);
+    }
+
+    private void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * 0.8f);
+    }
+
+    private void CreateAsteroids(float asteroidsNum)
+    {
+        for (int i = 1; i <= asteroidsNum; i++) {
+            GameObject asteroidsClone = Instantiate(asteroid, new Vector2(Random.Range(-10, 10), Random.Range(-10, 10)), transform.rotation);
+            // asteroidsClone.transform.parent = asteroidContainer.transform;
+            asteroidsClone.SetActive(true);
+        }
+
+    }
+
+     public void RocketFail()
+    {
+        print("ROCKET IS DEAD");
+
+    }
+
+
+
+}
---
+++ Asteroids/Assets/_Game/Scripts/Rocket.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public GameObject rockeEngine;
+    // public GameObject bulletContainer;
+
+    private ParticleSystem ps;
+    //private float thrust = 5f;
+    private float thrust = 6f;
+    private float rotationSpeed = 180f;
+    private float MaxSpeed = 4.5f;
+
+    private Camera mainCam;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        mainCam = Camera.main;
+        rb = GetComponent<Rigidbody>();
+
+        ps = rockeEngine.GetComponent<ParticleSystem>();
+        ps.Stop();
+
+        bullet.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        ControlRocket();
+        CheckPosition();
+
+        
+    }
+
+  
+    private void Update()
+    {
+       if (Input.GetMouseButtonDown(0) || Input.GetKeyDown("space"))
+        {
+            Shoot();
+        }
+
+        if (Input.GetKeyDown("up") || Input.GetKeyDown("w"))
+        {
+            ps.Play();
+        }
+        if (Input.GetKeyUp("up") || Input.GetKeyUp("w"))
+        {
+            ps.Stop();
+        } 
+    }
+  
+
+    private void ControlRocket()
+    {
+        transform.Rotate(0, 0, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
+        rb.AddForce(transform.up * thrust * Input.GetAxis("Vertical"));
+        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -MaxSpeed, MaxSpeed), Mathf.Clamp(rb.velocity.y, -MaxSpeed, MaxSpeed));
+    }
+
+    private void CheckPosition()
+    {
+
+        float sceneWidth = mainCam.orthographicSize * 2 * mainCam.aspect;
+        float sceneHeight = mainCam.orthographicSize * 2;
+
+        float sceneRightEdge = sceneWidth / 2;
+        float sceneLeftEdge = sceneRightEdge * -1;
+        float sceneTopEdge = sceneHeight / 2;
+        float sceneBottomEdge = sceneTopEdge * -1;
+
+        if (transform.position.x > sceneRightEdge)
+        {
+            transform.position = new Vector2(sceneLeftEdge, transform.position.y);
+        }
+        if (transform.position.x < sceneLeftEdge)
+        {
+            transform.position = new Vector2(sceneRightEdge, transform.position.y);
+        }
+        if (transform.position.y > sceneTopEdge)
+        {
+            transform.position = new Vector2(transform.position.x, sceneBottomEdge);
+        }
+        if (transform.position.y < sceneBottomEdge)
+        {
+            transform.position = new Vector2(transform.position.x, sceneTopEdge);
+        }
+
+    }
+
+    void Shoot()
+    {
+        GameObject bulletClone = Instantiate(bullet, new Vector2(bullet.transform.position.x, bullet.transform.position.y), transform.rotation);
+        bulletClone.SetActive(true);
+        bulletClone.GetComponent<Bullet>().KillOldBullet();
+        bulletClone.GetComponent<Rigidbody>().AddForce(transform.up * 350);
+    }
+}
