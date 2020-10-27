56e9363985c2b8a2cc3d4bf5dac32931fe1028da
Mikhail Sergeev <sergeevmisha16@gmail.com>
2020-10-27 21:58:02 +0300

    Update

---
+++ DOTS(ECS) course/Counting sheep/Assets/Scripts/Systems/MoveSystem.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class MoveSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var jobHandle = Entities
+            .WithName("MoveSystem")
+            .ForEach((ref Translation translation, ref Rotation rotation) =>
+            {
+                
+            })
+            .Schedule(inputDeps);
+        return jobHandle;
+    }
+}
