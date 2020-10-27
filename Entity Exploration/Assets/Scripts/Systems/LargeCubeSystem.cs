c1cd6ff4c41c31750e83ec9c91e84037b6470063
Mikhail Sergeev <sergeevmisha16@gmail.com>
2020-10-27 19:03:38 +0300

    First commit

---
+++ Entity Exploration/Assets/Scripts/Systems/LargeCubeSystem.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class LargeCubeSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var jobHandle = Entities
+            .WithName("MoveSystem")
+            .ForEach((ref Translation translation, ref Rotation rotation, ref LargeCubeData largeCubeData) =>
+            {
+                translation.Value -= 0.01f * math.up();
+            })
+            .Schedule(inputDeps);
+        return jobHandle;
+    }
+    
+}
