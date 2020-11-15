4514a40d8daee464e40ea13bc555f1c496cd7a20
Mikhail Sergeev <sergeevmisha16@gmail.com>
2020-11-15 14:57:08 +0300

    Create an object from scrutch and save it as a prefab

---
+++ Design Patterns course/The Prototype Pattern (Dynamic Prefab Creation)/Assets/Editor/AsteroidSpawnerEditor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+[CustomEditor (typeof(AsteroidSpawner))]
+public class AsteroidSpawnerEditor : Editor
+{
+    string path;
+    string assetpath;
+    string filename;
+
+    void OnEnable()
+    {
+        path = Application.dataPath + "/Asteroid";
+        assetpath = "Assets/Asteroid/";
+        filename = "asteroid_" + System.DateTime.Now.Ticks.ToString();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        AsteroidSpawner astSpawner = (AsteroidSpawner) target;
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Create Asteroid"))
+        {
+            astSpawner.CreateAsteroid();
+        }
+
+        if (GUILayout.Button("Save Asteroid"))
+        {
+            System.IO.Directory.CreateDirectory(path);
+            Mesh mesh = astSpawner.asteroid.GetComponent<MeshFilter>().sharedMesh;
+            AssetDatabase.CreateAsset(mesh, assetpath + mesh.name + ".asset");
+            AssetDatabase.SaveAssets();
+
+            PrefabUtility.SaveAsPrefabAsset(astSpawner.asteroid, assetpath + filename + ".prefab");
+        }
+    }
+}
---
+++ Design Patterns course/The Prototype Pattern (Dynamic Prefab Creation)/Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    public Material material;
+    public GameObject asteroid;
+
+
+
+    public void CreateAsteroid()
+    {
+        asteroid = ProcAsteroid.Clone(this.transform.position);
+        asteroid.GetComponent<MeshRenderer>().sharedMaterial = material;
+    }
+
+}
