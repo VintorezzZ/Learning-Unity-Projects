9ff898bc917f2b9f68e5cce556fe38962c0f457c
Mikhail Sergeev <sergeevmisha16@gmail.com>
2020-11-17 23:00:01 +0300

    add carwar mob

---
+++ CarWar mobile/Assets/Scripts/HealthPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPlayer : MonoBehaviour
+{
+    private int health;
+    public int maxHealth;
+    public ParticleSystem expl;
+
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GetDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            health = 0;            
+            Instantiate(expl, transform.position, transform.rotation);
+            Destroy(this.gameObject, 0f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Wall")
+        {
+            GetDamage(maxHealth);
+            Instantiate(expl, transform.position, transform.rotation);
+            Destroy(this.gameObject, 0f);
+            GameManager.instance.SetDeathUI();
+        }
+        else if (collision.transform.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<Health>().GetDamage(maxHealth);
+            GetDamage(maxHealth);
+            Instantiate(expl, transform.position, transform.rotation);
+            Destroy(this.gameObject, 0f);
+            GameManager.instance.SetDeathUI();
+        }    
+    }
+}
---
+++ CarWar mobile/Assets/Scripts/Machinegun.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Machinegun : MonoBehaviour
+{
+    public Transform gun;
+    public Transform barrel;
+    public float gunSpeed;
+    public float barrelSpeed;
+    private float gunAngle;
+    private float barrelAngle;
+    public GameObject bullet;
+    public Transform barrelL;
+    public Transform barrelR;
+    public float bulletSpeed;
+    private float nextTimeToFire;
+    private float fireRate = 5f;
+    public AudioClip shotSound;
+    public AudioSource audioSource;
+    public ParticleSystem shootVFX;
+
+    private int currentAmmo = 30;
+    private float timer;
+
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {        
+        //RotateGun();
+        //RotateBarrel();
+
+        //if (!GameManager.instance.pause)
+        //{
+        //    if (EventSystem.current.IsPointerOverGameObject())
+        //        return;
+
+        //    if (Time.time > nextTimeToFire)
+        //    {
+        //        nextTimeToFire = Time.time + 1f / fireRate;
+        //        if (currentAmmo > 0)
+        //        {
+        //            Shoot();
+        //        }                       
+
+        //    }
+
+        //}
+
+        UpdateAmmo();
+        GameManager.instance.UpdateAmmo(currentAmmo);
+    }
+
+    void RotateGun()
+    {
+        gunAngle += Input.GetAxis("Mouse X") * GameManager.instance.sens * Time.deltaTime;
+        gunAngle = Mathf.Clamp(gunAngle,-90,90);
+        gun.localRotation = Quaternion.AngleAxis(gunAngle, Vector3.up);
+    }
+
+    void RotateBarrel()
+    {
+        barrelAngle += Input.GetAxis("Mouse Y") * barrelSpeed * -Time.deltaTime;
+        barrelAngle = Mathf.Clamp(barrelAngle, -1, 20);
+        barrel.localRotation = Quaternion.AngleAxis(barrelAngle, Vector3.right);
+    }
+
+    void Shoot()
+    {        
+        GameObject bulletInstanceR;
+        GameObject bulletInstanceL;
+
+        //bulletInstanceR = Instantiate(bullet, barrelR.position, Quaternion.identity) as GameObject;
+        bulletInstanceL = Instantiate(bullet, barrelL.position, Quaternion.identity) as GameObject;
+
+        //Rigidbody bulletRbR;
+        Rigidbody bulletRbL;
+
+       // bulletRbR = bulletInstanceR.GetComponent<Rigidbody>();
+        bulletRbL = bulletInstanceL.GetComponent<Rigidbody>();
+
+       // bulletRbR.AddForce(barrelR.forward * bulletSpeed);
+        bulletRbL.AddForce(barrelL.forward * bulletSpeed);
+
+       // Destroy(bulletInstanceR, 10f);
+        Destroy(bulletInstanceL, 10f);
+
+        currentAmmo--;
+
+
+
+        audioSource.PlayOneShot(shotSound);
+        shootVFX.Play(shootVFX);
+    }
+
+    void UpdateAmmo()
+    {        
+        timer += Time.deltaTime;
+        if (timer >= 2.5f)
+        {
+            currentAmmo++;
+            timer = 0;
+        }
+
+        if (currentAmmo < 0)
+        {
+            currentAmmo = 0;
+        }
+    }
+
+}
