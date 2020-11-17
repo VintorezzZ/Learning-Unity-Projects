938381acf51d7661c5576f6f4dd6d1e998e470c8
Mikhail Sergeev <sergeevmisha16@gmail.com>
2020-11-17 22:56:19 +0300

    add carwar

---
+++ CarWar/Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    public static SpawnEnemies instance;
+
+    public List<Transform> spawnPool;
+    public List<GameObject> enemiesList;
+    //public List<GameObject> deadList;
+    public GameObject enemies;
+    private float timer = 10;
+
+
+    private void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    void Start()
+    {        
+        _SpawnEnemies();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0) 
+        {
+            timer = 10;
+            _SpawnEnemies();
+        }
+
+        GameManager.instance.UpdateTimer(timer);
+    }
+
+    void _SpawnEnemies()
+    {
+        for (int i = 0; i < spawnPool.Count; i++)
+        {
+            Instantiate(enemiesList[i], spawnPool[i].position, spawnPool[i].rotation);
+
+        }
+    }
+
+
+}
